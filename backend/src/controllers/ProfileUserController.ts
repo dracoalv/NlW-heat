4f1a9a07f71017ce019cc6a308866a3c866d085b
dracoalv <dracoalv@hotmail.com>
2021-10-19 23:28:40 -0300

    🔧 Started Stage 2

--- backend/src/controllers/ProfileUserController.ts
+++ backend/src/controllers/ProfileUserController.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from "express";
 import { ProfileUserService } from "../services/ProfileUserService";
 
-
-
 class ProfileUserController {
   async handle(request: Request, response: Response) {
     const { user_id } = request;
